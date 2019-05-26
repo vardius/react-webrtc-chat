f41a42860ebf8e297a972f6d3af53d3fa3b4aaa8
Rafał Lorenz <vardius@gmail.com>
2019-05-26 16:33:02 +1000

    Update test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { PeerDataProvider } from "react-peer-data";
+import { UserMediaProvider } from "@vardius/react-user-media";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <PeerDataProvider
+      servers={{
+        iceServers: [
+          {
+            url: "stun:stun.1.google.com:19302"
+          }
+        ]
+      }}
+      constraints={{ ordered: true }}
+      signaling={{
+        url:
+          process.env.NODE_ENV !== "production" ? "http://localhost:3001" : null
+      }}
+    >
+      <UserMediaProvider constraints={{ audio: true, video: true }}>
+        <App />
+      </UserMediaProvider>
+    </PeerDataProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
